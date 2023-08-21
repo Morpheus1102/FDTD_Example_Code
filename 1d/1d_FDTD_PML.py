c8069e84c454f55af8e58d06137226c52f0d39e8
Morpheus Yin <72175373+Morpheus1102@users.noreply.github.com>
2023-08-21 11:14:49 +0000

    Update 1d_FDTD_PML.py

--- 1d/1d_FDTD_PML.py
+++ 1d/1d_FDTD_PML.py
@@ -3,67 +3,67 @@ import matplotlib.pyplot as plt
 import imageio
 
 # Constants
-c = 1  # Speed of light
-epsilon = 1  # Permittivity
-mu = 1  # Permeability
+c = 3e+8
+mu0 = 4 * np.pi * 1e-7
+epsilon0 = (1 / (36 * np.pi)) * 1e-9
+S = 1 / (2**0.5)
+delta = 1e-6
+deltat = S * delta / c
 
 # Grid parameters
-nx = 400  # Number of cells
-dx = 1  # Cell size
-dt = dx / (2 * c)  # Time step
-bound_width = 50  # PML boundary width
-gradingorder = 6  # Order of polynomial grading
-refl_coeff = 1e-6  # Reflection coefficient
-sigmamax = (-np.log10(refl_coeff) * (gradingorder + 1) * epsilon * c) / (2 * bound_width * dx)
+xdim = 500
+nt = 500
 
 # Arrays for fields
-Ex = np.zeros(nx)
-Hy = np.zeros(nx)
-sigma = np.zeros(nx)
-sigma_star = np.zeros(nx)
-
-# PML sigma calculations
-boundfact1 = sigmamax / ((bound_width ** gradingorder) * (gradingorder + 1))
-boundfact2 = sigmamax / ((bound_width ** gradingorder) * (gradingorder + 1))
-x = np.arange(bound_width)
-sigma[:bound_width] = boundfact1 * ((x + 0.5) ** (gradingorder + 1) - (x - 0.5) ** (gradingorder + 1))
-sigma[-bound_width:] = boundfact2 * ((x[::-1] + 0.5) ** (gradingorder + 1) - (x[::-1] - 0.5) ** (gradingorder + 1))
-sigma_star = (sigma * mu) / epsilon
-
-# Multiplication factors
-A = (mu - 0.5 * dt * sigma_star) / (mu + 0.5 * dt * sigma_star)
-B = (dt / dx) / (mu + 0.5 * dt * sigma_star)
-C = (epsilon - 0.5 * dt * sigma) / (epsilon + 0.5 * dt * sigma)
-D = (dt / dx) / (epsilon + 0.5 * dt * sigma)
+Ez = np.zeros(xdim)
+Hy = np.zeros(xdim)
 
 # Simulation parameters
-nt = 500  # Number of time steps
-source_position = nx // 2  # Position of the source
+epsilon = epsilon0 * np.ones(xdim)
+mu = mu0 * np.ones(xdim)
 
-# Create a writer object
+# PML parameters
+sigma = np.zeros(xdim)
+bound_width = 25
+gradingorder = 6
+refl_coeff = 1e-6
+sigmamax = (-np.log10(refl_coeff) * (gradingorder + 1) * epsilon0 * c) / (2 * bound_width * delta)
+
+for i in range(bound_width):
+    sigma_value = sigmamax * ((bound_width - i - 0.5) ** gradingorder) / bound_width ** gradingorder
+    sigma[i] = sigma_value
+    sigma[-i - 1] = sigma_value
+
+sigma_star = (sigma * mu) / epsilon
+A = (epsilon - 0.5 * deltat * sigma) / (epsilon + 0.5 * deltat * sigma)
+B = (deltat / delta) / (epsilon + 0.5 * deltat * sigma)
+C = (mu - 0.5 * deltat * sigma_star) / (mu + 0.5 * deltat * sigma_star)
+D = (deltat / delta) / (mu + 0.5 * deltat * sigma_star)
+
+# Source position
+source_position = xdim // 2
+
 with imageio.get_writer('1d_fdtd_pml_simulation.gif', mode='I') as writer:
-    # Main FDTD loop
     for t in range(nt):
-        # Update magnetic field with PML
-        Hy[:-1] = A[:-1] * Hy[:-1] + B[:-1] * (Ex[1:] - Ex[:-1])
-
         # Gaussian source
-        source_amplitude = 0.3
-        source = source_amplitude * np.exp(-0.5 * ((t - 30) / 10) ** 2)
-        Hy[source_position] += source
+        source = np.exp(-0.5 * ((t - 30) / 10) ** 2)
+        Ez[source_position] += source
 
-        # Update electric field with PML
-        Ex[1:] = C[1:] * Ex[1:] + D[1:] * (Hy[1:] - Hy[:-1])
+        # Update magnetic field
+        Hy[:xdim - 1] = C[:xdim - 1] * Hy[:xdim - 1] + D[:xdim - 1] * (Ez[1:xdim] - Ez[:xdim - 1])
+
+        # Update electric field
+        Ez[1:xdim] = A[1:xdim] * Ez[1:xdim] + B[1:xdim] * (Hy[1:xdim] - Hy[:xdim - 1])
 
         # Plot the electric field at each time step
-        if t % 10 == 0:
-            plt.plot(Ex)
-            plt.xlabel('Grid Index')
-            plt.ylabel('Electric Field (Ex)')
+        if t % 5 == 0:
+            plt.plot(Ez)
+            plt.ylim(-1.5, 1.5)
             plt.title('1D FDTD with PML at t = ' + str(t))
-            plt.ylim([-1.2, 1.2])
-            
+            plt.xlabel('Grid Point')
+            plt.ylabel('Electric Field')
+
             # Convert plot to image and append to GIF
-            plt.savefig('temp.png')
+            plt.savefig('temp.png', bbox_inches='tight', pad_inches=0)
             writer.append_data(imageio.imread('temp.png'))
             plt.close()
